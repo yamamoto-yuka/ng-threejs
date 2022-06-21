2844e330da0cb4f52fffea0d2cae553a4fa07e05
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-06-20 19:07:40 -0700

    Created earth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Host, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EarthComponent } from './earth/earth.component';
 import { HomeComponent } from './home/home.component';
 import { ThreeComponent } from './three/three.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'cube', component:ThreeComponent}
+  {path:'cube', component:ThreeComponent},
+  {path:'earth', component:EarthComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThreeComponent } from './three/three.component';
 import { HomeComponent } from './home/home.component';
+import { EarthComponent } from './earth/earth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ThreeComponent,
-    HomeComponent
+    HomeComponent,
+    EarthComponent
   ],
   imports: [
     BrowserModule,
